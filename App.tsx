ce71f9385de4310a2f1010736a7171a74b449757
Zachary Williams <kcaz311@gmail.com>
2021-08-06 10:02:59 -0400

    init beta branch

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
+        
         <StatusBar />
       </SafeAreaProvider>
     );
